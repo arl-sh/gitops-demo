dbaad0b29679ca0531a954716ed80a13de72b467
Aurélien GARNIER <aurelien.garnier@atos.net>
2021-12-10 13:25:44 +0100

    Delete gitops cluster

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-CLUSTER_NAME=gitops
+CLUSTER_NAME="gitops"
+CLUSTER_PATH="./clusters/staging"
 GITHUB_TOKEN="ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 GITHUB_USERNAME="au2001"
 GITHUB_REPOSITORY="gitops-demo"
 
 cd `dirname "$0"`
 
-kind delete cluster --name="$CLUSTER_NAME"
+if [ "$1" == "reset" ]; then
+    kind delete cluster --name="$CLUSTER_NAME"
 
-kind create cluster --config kind-cluster.conf --name="$CLUSTER_NAME"
+    kind create cluster --config kind-cluster.conf --name="$CLUSTER_NAME"
+fi
 
-GITHUB_TOKEN="$GITHUB_TOKEN" flux bootstrap github --owner="$GITHUB_USERNAME" --repository="$GITHUB_REPOSITORY" --branch=main --path="./clusters/$CLUSTER_NAME" --personal --token-auth
+GITHUB_TOKEN="$GITHUB_TOKEN" flux bootstrap github --context="kind-$CLUSTER_NAME" --owner="$GITHUB_USERNAME" --repository="$GITHUB_REPOSITORY" --branch=main --path="$CLUSTER_PATH" --personal --token-auth
