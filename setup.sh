7d6319340d7ab1f20795b3889799ecdcd5346453
Aurélien GARNIER <au2001sfg@gmail.com>
2022-01-04 17:59:07 +0100

    Add secrets

--- setup.sh
+++ setup.sh
@@ -23,3 +23,8 @@ GITHUB_TOKEN="$GITHUB_TOKEN" flux bootstrap github \
   --path="$CLUSTER_PATH" \
   --personal \
   --token-auth
+
+kubectl delete secret sops-gpg --namespace=flux-system
+
+GPG_KEY_FINGERPRINT=`gpg --show-keys --with-fingerprint --with-colons secrets/.sops.pub.asc | awk -F: '$1 == "fpr" {print $10;}' | head -n 1`
+gpg --export-secret-keys --armor "$GPG_KEY_FINGERPRINT" | kubectl create secret generic sops-gpg --namespace=flux-system --from-file=sops.asc=/dev/stdin
